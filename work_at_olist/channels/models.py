aa8b5501777a971c49406695ee20924b55ffb47d
chico <fxchiang@gmail.com>
2017-11-05 14:44:22 -0200

    added factory methods to Channel and Category, automatically add categories root to Channel

--- work_at_olist/channels/models.py
+++ work_at_olist/channels/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Category(MPTTModel):
     name = models.CharField(
         max_length=255,
-        null=False,
-        blank=False
     )
     parent = TreeForeignKey(
         'self',
@@ -26,23 +26,37 @@ class Category(MPTTModel):
         unique_together = (('name', 'parent'),)
         verbose_name_plural = 'categories'
 
+    @classmethod
+    def create(cls, name, parent=None):
+        return cls.objects.create(name=name, parent=parent)
+
 
 class Channel(models.Model):
+    categories = models.OneToOneField(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='root_of',
+    )
     name = models.CharField(
         max_length=255,
         null=False,
         blank=False,
         unique=True,
     )
-    categories = models.OneToOneField(
-        Category,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE,
-        primary_key=True,
-        related_name='root_of',
-    )
 
     def __str__(self):
         return self.name
 
+    @classmethod
+    def create(cls, name):
+        cls.objects.create(name=name)
+
+
+@receiver(pre_save, sender=Channel)
+def create_root_categories(sender, instance, **kwargs):
+    '''
+    add categories root if it doesnt exist
+    '''
+    if not hasattr(instance, 'categories'):
+        cat = Category.create(instance.name)
+        instance.categories = cat
