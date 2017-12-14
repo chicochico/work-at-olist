b7ebeea99c3463821a5af4aa51bb78851dc67e62
chico <fxchiang@gmail.com>
2017-12-14 16:05:46 -0200

    use django rest swagger for api documentation

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
 from api.views import ChannelViewSet, CategoryViewSet, SearchViewSet
 
 
+schema_view = get_swagger_view(title='Channels API')
 router = routers.DefaultRouter()
 router.register(r'channels', ChannelViewSet, 'channel')
 router.register(r'categories', CategoryViewSet, 'category')
@@ -11,6 +12,6 @@ router.register(r'search', SearchViewSet, 'search')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^docs/', include_docs_urls(title='Sellers Channels API'), name='api-docs'),
+    url(r'^docs/', schema_view),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- work_at_olist/settings/base.py
+++ work_at_olist/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'mptt',
     'rest_framework',
+    'rest_framework_swagger',
     'channels',
     'api',
 ]
